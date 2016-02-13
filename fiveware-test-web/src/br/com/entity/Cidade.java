eba8ab0e4d719c19aad268f86d787b3391a94d11
Bruna <Bruna@Bruna-PC>
2016-02-13 10:50:36 -0200

    nome

--- fiveware-test-web/src/br/com/entity/Cidade.java
+++ fiveware-test-web/src/br/com/entity/Cidade.java
@@ -1,5 +1,18 @@
 package br.com.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Cidade {
+	
+	@Id 
+	@GeneratedValue
+	@Getter	@Setter
+	private int id;
 
 }
