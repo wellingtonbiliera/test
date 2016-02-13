79ddd1aff823b538a854a3b5ecec6318a10c9566
Bruna <Bruna@Bruna-PC>
2016-02-13 20:45:35 -0200

    integração de projetos

--- fiveware-test-service/src/com/service/PessoaImpl.java
+++ fiveware-test-service/src/com/service/PessoaImpl.java
@@ -2,21 +2,19 @@ package com.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.dao.PessoaDAO;
 import br.com.entity.Pessoa;
 
 @Service
 public class PessoaImpl implements PessoaService {
 	
-	@Autowired
-	private PessoaDAO dao;
+//	@Autowired
+//	private PessoaDAO dao;
 	
 	@Override
 	public void save(Pessoa p) {
-		dao.insert(p);
+		//dao.insert(p);
 	}
 
 	@Override
@@ -26,7 +24,7 @@ public class PessoaImpl implements PessoaService {
 
 	@Override
 	public Pessoa buscaPessoa(Integer id) {
-		return dao.buscaPessoa(id);
+		return null;//dao.buscaPessoa(id);
 	}
 	
 	
--- fiveware-test-web/WebContent/index.jsp
+++ fiveware-test-web/WebContent/index.jsp
@@ -1,10 +1,9 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<script src="jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
 <title>Cadastro Simples</title>
 
 </head>
